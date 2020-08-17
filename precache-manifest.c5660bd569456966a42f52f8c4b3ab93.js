ebe14cd3bae7f1c258fb6cbd077124a0440571c1
Dmytro Hoi <code@dmytrohoi.com>
2020-08-17 15:14:09 +0300

    Updates

---
+++ precache-manifest.c5660bd569456966a42f52f8c4b3ab93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958f14c462054008848f9c93ade0a3b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07dc6fd55f3ef686460c",
+    "url": "/static/css/2.25344b1b.chunk.css"
+  },
+  {
+    "revision": "c8fffae840de4c963b90",
+    "url": "/static/css/main.668bb2df.chunk.css"
+  },
+  {
+    "revision": "07dc6fd55f3ef686460c",
+    "url": "/static/js/2.6f94cd8a.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.6f94cd8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8fffae840de4c963b90",
+    "url": "/static/js/main.08a769dc.chunk.js"
+  },
+  {
+    "revision": "e9a7bed6fbbb0a7a1370",
+    "url": "/static/js/runtime-main.f3c87f5d.js"
+  }
+]);
